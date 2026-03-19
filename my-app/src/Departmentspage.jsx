3dc29f88f44988758ce866e7d37779f175c82993
JoeJoe08 <phyoekyawzin10@gmail.com>
2026-03-19 20:18:47 +0700

    Update Departmentspage.jsx

--- my-app/src/Departmentspage.jsx
+++ my-app/src/Departmentspage.jsx
@@ -1,5 +1,5 @@
 // src/pages/DepartmentsPage.jsx
-import { Avatar, PageHeader, Button } from "./ui";
+import { Avatar, PageHeader, Button } from "./Ui";
 
 const DEPT_COLORS = { Engineering: "#4f46e5", HR: "#be185d", Marketing: "#0369a1", Finance: "#15803d" };
 
